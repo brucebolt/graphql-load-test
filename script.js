2a3608ed5c7183b7b979c9524287912dd13cd07b
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2024-11-20 13:31:17 +0000

    Update load testing script
    Now 3000 requests over 30 seconds (so average 100 per second).
    
    Also reports on response codes.

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
-  vus: 25,
+  vus: 3000,
   // A string specifying the total duration of the test run.
   duration: '30s',
 }
 
 export default function() {
-  const res = http.get('https://www-origin.staging.publishing.service.gov.uk/world');
+  const res = http.get('http://collections.apps.svc.cluster.local/world');
 
   check(res, {
     'is status 200': (r) => r.status === 200,
+    'is status 2xx, not 200': (r) => r.status >= 201 && r.status <= 299,
+    'is status 3xx': (r) => r.status >= 300 && r.status <= 399,
+    'is status 4xx': (r) => r.status >= 400 && r.status <= 499,
+    'is status 5xx': (r) => r.status >= 500 && r.status <= 599,
   });
+
+  sleep(1);
 }
