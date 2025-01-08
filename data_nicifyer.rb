73d6e4de2451ffc71d60d2b88b9bae4709d5d2d9
Mike Patrick <mike.patrick2@digital.cabinet-office.gov.uk>
2025-01-08 14:41:00 +0000

    Row data

--- data_nicifyer.rb
+++ data_nicifyer.rb
@@ -2,12 +2,12 @@ require "json"
 require "csv"
 
 data = JSON.parse(File.read("data.json"))
-names = data["pages"].map{|x| x["name"]}
+page_names = data["pages"].map{|x| x["name"]}
 vuss = data["vus"]
 
 
 
-title_place_mashup_hash = {
+TITLE_PLACE_MASHUP_HASH = {
   "request_count" =>  ["metrics", "http_reqs", "count"],
   "request_duration_avg" => ["metrics", "http_req_duration", "avg"],
   "request_duration_p90" => ["metrics", "http_req_duration", "p(90)"],
@@ -15,13 +15,25 @@ title_place_mashup_hash = {
   "failed_request_count" => ["metrics", "http_req_failed", "passes"], #this is correct because k6 loves the weird double negative of a failure pass
 }
 
+def row_data(page_name, vus, graphql: false)
+  filename = if graphql
+               "summaries/#{page_name}_#{vus}_graphql_summary.json"
+             else
+               "summaries/#{page_name}_#{vus}_summary.json"
+             end
+
+  summary_data = JSON.parse(File.read(filename))
+
+  [page_name, vus, graphql] +
+    TITLE_PLACE_MASHUP_HASH.map { |_, dig_path| summary_data.dig(*dig_path) }
+end
+
 CSV.open("summaries/data_amalgam.csv", "w") do |csv|
-  csv << ["page_name", "vus", "graphql?"] + title_place_mashup_hash.keys
-  names.each do |name|
+  csv << ["page_name", "vus", "graphql?"] + TITLE_PLACE_MASHUP_HASH.keys
+  page_names.each do |page_name|
     vuss.each do |vus|
-      
-      scenarios << "#{name}_#{vus}"
-      scenarios << "#{name}_#{vus}_graphql"
+      csv << row_data(page_name, vus)
+      csv << row_data(page_name, vus, graphql: true)
     end
   end
 end
